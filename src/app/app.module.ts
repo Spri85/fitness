45f244bec6e60061ba0d5c7c3f3834f5d4ea4e20
Viktor <viktorpar@gmail.com>
2018-10-08 11:28:58 +0300

    init auth modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
+import { Store } from 'store';
+
+// feature modules
+import { AuthModule } from './auth/auth.module';
+
+// containers
 import { AppComponent } from './app.component';
 
+// components
+
+// routes
+export const ROUTES: Routes = [];
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  declarations: [AppComponent],
+  imports: [BrowserModule, RouterModule.forRoot(ROUTES)],
+  providers: [Store],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
+
+/* 
+var config = {
+    apiKey: "AIzaSyB48x09FCtpOv0fhGSCPp3-zr78-4D71zM",
+    authDomain: "fitness-app-832f8.firebaseapp.com",
+    databaseURL: "https://fitness-app-832f8.firebaseio.com",
+    projectId: "fitness-app-832f8",
+    storageBucket: "fitness-app-832f8.appspot.com",
+    messagingSenderId: "490368438107"
+  };
+*/
